e0def03d1f7172f731fbb023334d127b101f6aec
Aya <ao2368@nyu.edu>
2025-01-19 21:22:03 -0500

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/aya0221.github.io",
-  assetPrefix: "/aya0221.github.io",
+  assetPrefix: "/aya0221.github.io/",
   trailingSlash: true,
-  output: "export",
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
