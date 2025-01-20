2d4d1a536403c228b43ed6555ed6ccc8d2c39d0f
Aya <ao2368@nyu.edu>
2025-01-19 21:08:22 -0500

    theme-provider use client fix

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+'use client';
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>; // Replace with actual theme logic if required
+import { ThemeProvider as NextThemesProvider, Attribute } from 'next-themes';
+import { ReactNode } from 'react';
+
+interface ThemeProviderProps {
+  children: ReactNode;
+  attribute?: Attribute | Attribute[] | undefined;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
 }
 
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
